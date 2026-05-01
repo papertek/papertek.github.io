bb7d33aa68fddaa2d61f91903017081779342439
papertek <littlerock894@gmail.com>
2026-04-30 20:26:18 -0500

    fix: change banner

--- src/lib/components/DevBanner.svelte
+++ src/lib/components/DevBanner.svelte
@@ -1,6 +1,5 @@
 <div class="sticky bottom-0 left-0 z-100 bg-black text-sm">
     <div class="flex w-full items-end justify-end gap-2 py-1 pr-4 text-white">
-        Development build of NetroWorks. This is made for desktop, do not test on mobile yet. Expect broken things and make sure to give us
-        feedback!
+        This is a test environment for unicorn effects. Please let us know about any performance issues. Thank you!
     </div>
 </div>
